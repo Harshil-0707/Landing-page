462f78061164b514664df00d1b16a16969e9e0aa
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2023-12-31 21:59:46 +0530

    added some styles and content and logic

---
+++ main.js
@@ -0,0 +1,11 @@
+const myEmail = document.getElementById('myEmail');
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+document.getElementById('submit').onclick = () => {
+    if(!emailPattern.test(myEmail.value)){
+        document.getElementById('error').style.display = "block";
+    }else{
+        document.getElementById('error').style.display = "none";
+
+    }
+};
